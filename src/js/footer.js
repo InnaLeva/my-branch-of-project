c8e87e7b70ee3cdcfd5e780c502c37f8755ffd14
Inna Ratushna <Ratushnaya.uv@gmail.com>
2024-08-19 00:36:55 +0300

    add some code

---
+++ src/js/footer.js
@@ -0,0 +1,5 @@
+
+const form = document.querySelector('.footer-form');
+const input = document.querySelector('.footer-form-label-input');
+const sendBtn = document.querySelector('.footer-form-button');
+
