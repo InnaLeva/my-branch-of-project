b31ce12eac1cb25a90dbd14a80c9052edd48200b
Inna Ratushna <Ratushnaya.uv@gmail.com>
2024-08-20 11:31:54 +0300

    fixed js

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,21 +1,125 @@
 
-import SimpleLightbox from "simplelightbox";
- import "simplelightbox/dist/simple-lightbox.min.css";
+
+import axios from 'axios';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector('.footer-form');
-const input = document.querySelector('.footer-form-label-input');
-const openModalBtn = document.querySelector('.footer-form-button');
-const closeModalBtn = document.querySelector('.footer-close-button');
+const emailInput = document.getElementById('email');
+const commentsInput = document.querySelector('.form-comments');
+const message = document.getElementById('emailSuccessMessage');
 
+const openModalBtn = document.querySelector('.footer-form-button');
+const charCount = document.getElementById('charCount');
+
+
+const closeModalBtn = document.querySelector('.footer-close-button');
 const footerModal = document.querySelector('.footer-backdrop');
 const modalContent = document.querySelector('.footer-modal');
+const modalTitle = document.querySelector('.footer-backdrop-title');
+const modalText = document.querySelector('.footer-backdrop-text');
+
+
+emailInput.addEventListener('blur', validateEmail);
+
+commentsInput.addEventListener('input', () => {
+  const currentLength = commentsInput.value.length;
+
+  if (window.matchMedia('(min-width: 768px)').matches) {
+    charCount.style.display = 'block';
+  }
+
+  charCount.textContent = `${currentLength}`;
+
+  validateEmail();
+});
+
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  charCount.style.display = 'none';
+
+  modalContent.classList.remove('is-open');
+  footerModal.classList.remove('is-open');
+
+  axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api';
+
+  axios.post('/requests', {
+    email: emailInput.value.trim(),
+    comment: commentsInput.value.trim()
+  })
+    .then(response => {
+      modalTitle.textContent = response.data.title;
+      modalText.textContent = response.data.message;
+    })
+    .catch((error) => {
+      iziToast.show({
+          message: error.response?.data?.message || error.message || 'An error occurred',
+          backgroundColor: "#ef4040",
+          position: "topRight",
+          messageSize: 16,
+          messageColor: '#fff',
+          messageLineHeight: "150%",
+
+          timeout: 4000
+        });
+    })
+    .finally(() => {
+      form.reset();
+      charCount.textContent = '0';
+      emailInput.classList.remove('error');
+      emailInput.classList.remove('success');
+      message.textContent = '';
+      openModalBtn.disabled = true;
+    })
+
+})
 
 
 
-const STORAGE_KEY = "feedback-form-state";
+function validateEmail() {
+  if (emailInput.checkValidity() && emailInput.value.trim() !== "") {
+    emailInput.classList.remove('error');
+    emailInput.classList.add('success');
+    message.textContent = 'Success!';
+    message.style.color = '#3cbc81';
+
+    }  else  {
+    emailInput.classList.remove('success');
+    emailInput.classList.add('error');
+    message.textContent = 'Invalid email, try again';
+    message.style.color = '#e74a3b';
+  }
+
+    const isEmailValid = emailInput.checkValidity() && emailInput.value.trim() !== "";
+    const isCommentFilled = commentsInput.value.trim() !== "";
+    openModalBtn.disabled = !(isEmailValid && isCommentFilled);
+}
+
+openModalBtn.addEventListener('click', () => {
+  modal.classList.remove('.is-open')
+})
+
+const closeModal = () => {
+  footer-modal.classList.add("is-open");
+  footer-backdrop.classList.add("is-open");
+};
+
+closeModalBtn.addEventListener('click', closeModal);
+
+footer-backdrop.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
+
+/*const STORAGE_KEY = "feedback-form-state";
 
 let formData = {
     email: "",
+
     message: ""
 };
 
@@ -26,7 +130,7 @@ form.addEventListener("submit", handleFormSubmit);
 form.addEventListener("input", handleFormInput);
 
 // Функція для обробки введення даних у форму
-function handleFormInput(event) {
+export function handleFormInput(event) {
     const value = event.target.value.trim();
     const key = event.target.name.trim();
 
@@ -37,7 +141,7 @@ function handleFormInput(event) {
     console.log(key, value);
 }
 
-function populateForm() {
+export function populateForm() {
   let savedFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY));
   
   if (!savedFeedbackData) {
@@ -50,7 +154,7 @@ function populateForm() {
   }
 }
 
-function handleFormSubmit(event) {
+export function handleFormSubmit(event) {
     event.preventDefault();
 
     if (!formData.email || !formData.message) {
@@ -60,9 +164,9 @@ function handleFormSubmit(event) {
     localStorage.removeItem(STORAGE_KEY)
 
     event.currentTarget.reset()
-}
+}*/
 /*відкрити модальне вікно*/
-openModalBtn.addEventListener('click', function () {
+/*openModalBtn.addEventListener("click", function () {
     footerModal.classList.add("is-open")
 })
 //закрити модільне вікно
@@ -83,7 +187,7 @@ document.querySelector('.footer-backdrop .footer-modal').addEventListener('click
 footerModal.addEventListener('click', event => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove('is-open');
-});
+});*/
   
   
 
